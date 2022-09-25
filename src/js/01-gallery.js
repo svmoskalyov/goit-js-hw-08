33be3421377d160d8a6ffad9956cd2a6c8067035
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-09-25 16:38:10 +0300

    Завдання 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const galleryBox = document.querySelector('.gallery');
+
+makeGalleryMarkup(galleryItems);
+
+function makeGalleryMarkup(element) {
+  const markup = element
+    .map(({ preview, original, description }) => {
+      return /*html*/ `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}">
+    </a>`;
+    })
+    .join('');
+
+  galleryBox.insertAdjacentHTML('beforeend', markup);
+}
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
