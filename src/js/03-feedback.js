bd34a2e6edff3235a2245c1e977b995b3c8cddf4
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-09-26 15:32:58 +0300

    Створює слухачі

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,3 +1,19 @@
 import { load, save, remove } from './storage';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+feedbackForm.addEventListener('input', onInput);
+feedbackForm.addEventListener('submit', onFormSubmit);
+
+
+function onInput(arguments) {
+  // body
+}
+
+function onFormSubmit(arguments) {
+  // body
+}
+
+
 
