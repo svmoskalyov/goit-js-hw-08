31dc57c46856377de61447fb5c47e5dde1027fe5
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-09-26 17:36:56 +0300

    task02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,22 @@
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
-import player from '@vimeo/player';
+import { load, save } from './storage';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+setCurrentTime();
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  save(STORAGE_KEY, seconds);
+}
+
+function setCurrentTime() {
+  if (!load(STORAGE_KEY)) {
+    return;
+  }
+
+  player.setCurrentTime(load(STORAGE_KEY));
+}
